e59ab6c89d351b654d8c168be940943b993ee58c
Mehmet (P.R.M.) Misset <mehmet.misset@akzonobel.com>
2025-02-13 16:08:47 +0100

    Adding Depolyment to database of metadata

---
+++ demo-analytic-data-platform/2-meta-data-definitions/9-Depoyment/1-Scripts/3-documentation/Script.usp_build_html_file_dataset_all.sql
@@ -0,0 +1,40 @@
+﻿
+DROP PROCEDURE IF EXISTS ##build_html_file_dataset_all;
+GO
+CREATE PROCEDURE ##build_html_file_dataset_all
+
+    /* Input Parameters */
+    @ip_is_debugging     BIT = 0,
+    @ip_is_testing       BIT = 0
+
+AS DECLARE 
+      
+    /* Data Attributes */
+    @id_dataset CHAR(32)
+
+BEGIN
+    
+  IF (1=1 /* Extract all "datasets". */) BEGIN
+
+    DROP TABLE IF EXISTS ##dst; SELECT dst.id_dataset
+    INTO ##dst FROM tsa_dta.tsa_dataset AS dst
+    WHERE NOT nm_target_schema IN ('dq_result', 'dq_totals', 'dta_dq_aggregates', 'dqm');
+
+  END
+
+  WHILE ((SELECT COUNT(*) FROM ##dst) > 0) BEGIN
+
+	  /* Extract all "Transformation"-parts from "source"-query. */
+    EXEC ##build_html_file_dataset
+      @ip_id_dataset   = @id_dataset,
+      @ip_is_debugging = @ip_is_debugging;
+
+    /* Remove processed "dataset". */
+    DELETE FROM ##dst
+    WHERE id_dataset = @id_dataset;
+
+  END
+  
+END
+GO
+
