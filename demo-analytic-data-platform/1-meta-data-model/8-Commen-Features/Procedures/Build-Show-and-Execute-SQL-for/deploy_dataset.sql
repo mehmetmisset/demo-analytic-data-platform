1d634ad005f8b3e89036a40892ed4139f79c01dc
Mehmet (P.R.M.) Misset <mehmet.misset@misset-data-analytics.nl>
2025-03-14 10:23:38 +0100

    updates

---
+++ demo-analytic-data-platform/1-meta-data-model/8-Commen-Features/Procedures/Build-Show-and-Execute-SQL-for/deploy_dataset.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [mdm].[deploy_dataset]
+
+  /* Input Parameters */
+  @ip_id_dataset       CHAR(32),
+  @ip_nm_target_schema NVARCHAR(128),
+  @ip_nm_target_table  NVARCHAR(128),
+
+  /* Input Paramters for "Debugging". */
+  @ip_is_debugging     BIT = 0,
+  @ip_is_testing       BIT = 0
+
+AS BEGIN
+
+  /* Local Variables */
+
+
+
+  SELECT @param1, @param2
+RETURN 0
