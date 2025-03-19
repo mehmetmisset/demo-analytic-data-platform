64eb27c56931cb49bcd671380d83ef59088a37f0
Mehmet (P.R.M.) Misset <mehmet.misset@misset-data-analytics.nl>
2025-03-19 16:59:16 +0100

    Update main.py

--- demo-analytic-data-platform/4-processing-python/main.py
+++ demo-analytic-data-platform/4-processing-python/main.py
@@ -8,6 +8,7 @@ import modules.run as run
 is_debugging = "1"
 
 # Process all datasets
+run.process('psa_yahoo_stocks',        'o', is_debugging)
 run.process('psa_yahoo_stocks',        'nvidia', is_debugging)
 run.process('psa_yahoo_stocks',        'abnas', is_debugging)
 run.process('psa_yahoo_exchange_rate', 'eur_x_cad', is_debugging)
